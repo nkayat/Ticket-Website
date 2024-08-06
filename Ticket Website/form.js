d7b7ba6fc9fa35b65d9bceef0346938a7d80a32c
nitin kayat <nkayat010@gamil.com>
2024-08-06 11:16:56 +0530

    add form page

---
+++ Ticket Website/form.js
@@ -0,0 +1,9 @@
+const continueFirst=document.getElementById("fullName2")
+function continueForm() {
+    if(continueFirst==''){
+        alert("please fill the name first");
+    }
+    else{
+        alert("your data was saved");
+    }
+}
